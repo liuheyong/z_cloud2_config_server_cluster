1344dd97d96ad39fbb8b925d451fc0d0eeb9f794
liuheyong <17682347237@163.com>
2019-06-13 17:41:26 +0800

    commit

---
+++ src/main/java/com/cloud2/configclu/ZCloud2ConfigServerClusterApplication.java
@@ -0,0 +1,13 @@
+package com.cloud2.configclu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZCloud2ConfigServerClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZCloud2ConfigServerClusterApplication.class, args);
+    }
+
+}
